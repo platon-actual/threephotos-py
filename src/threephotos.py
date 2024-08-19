0dd3767fd53bf730069bad8df9a780695a68d422
Ramiro Ivan Rios <ramiro.ivan.rios@gmail.com>
2024-08-19 19:10:01 -0300

    Se modifica para que tome 3 imágenes.

--- src/threephotos.py
+++ src/threephotos.py
@@ -1,7 +1,7 @@
 # Ramiro Iván Ríos (2024)
 
 from flask import Flask, request, render_template, url_for
-import os
+import os, shutil
 
 IMAGES_FOLDER = 'static/downloads/images/'
 
@@ -17,21 +17,22 @@ def show_images():
 
 @app.route('/post_image', methods=['POST'])
 def upload_image():
-    file = request.files['image']
-    
+    image = request.files['image']
+    print (image.content_type)
     #Vemos, en orden del 1 al 3, si existen las imágenes
     # Si existe, sigo a la siguiente, sino existe, la guardo allí.
-    # TODO que mueva las imágenes como un caroussel, la índice 1 al 2, la 2 al 3, y la 1 la imágen nueva
-    if (not image_exists(NAME_IMAGE_1)):
-        destination_path = os.path.join(app.root_path, IMAGES_FOLDER + NAME_IMAGE_1)
-        file.save(destination_path)
-    elif (not image_exists(NAME_IMAGE_2)):
-        destination_path = os.path.join(app.root_path, IMAGES_FOLDER + NAME_IMAGE_2)
-        file.save(destination_path)
-    elif (not image_exists(NAME_IMAGE_3)):
-        destination_path = os.path.join(app.root_path, IMAGES_FOLDER + NAME_IMAGE_3)
-        file.save(destination_path)
+    # # TODO que mueva las imágenes como un caroussel, la índice 1 al 2, la 2 al 3, y la NUEVA a la 1
+    # if (not image_exists(NAME_IMAGE_1)):
+    #     save_image(NAME_IMAGE_1, image)
+    # elif ((not image_exists(NAME_IMAGE_2)) and (image_exists[NAME_IMAGE_3])):
+    #     save_image(NAME_IMAGE_2, image)
+    # elif (not image_exists(NAME_IMAGE_3)):
+    #     save_image(NAME_IMAGE_3, image)
     
+    if (move_images(image)):
+        print("EXITOOOOOOO!!!!!!!!!")
+    else:
+        print("----- A REVISAR código -----")
     return url_for('show_images')
 
 @app.route('/test')
@@ -46,12 +47,44 @@ def test():
     
 def image_exists(filename):
     destination_path = os.path.join(app.root_path, IMAGES_FOLDER + filename)
-
     # Si existe la imagen 'filename' entonces devuelvo Verdadero. Sino, por defecto devuelve False.
     if( os.path.exists(destination_path) ):
         return True;
     return False;
 
+def save_image(name, file):
+    destination_path = os.path.join(app.root_path, IMAGES_FOLDER + name)
+    file.save(destination_path)
+
+def copy_image(origin, destination):
+    origin_path = os.path.join(app.root_path, IMAGES_FOLDER + origin)
+    destination_path = os.path.join(app.root_path, IMAGES_FOLDER + destination)
+    shutil.copy(origin_path, destination_path)
+
+def move_images(new_image):
+    # Si hay 3 imágenes:
+    if image_exists(NAME_IMAGE_3):
+        # Si hay 2 imágenes.. (ver else:)
+        if image_exists(NAME_IMAGE_2):
+            copy_image(NAME_IMAGE_2, NAME_IMAGE_3)
+            copy_image(NAME_IMAGE_1, NAME_IMAGE_2)
+            save_image(NAME_IMAGE_1, new_image)
+            return True
+        else:
+            if not image_exists(NAME_IMAGE_1):
+                save_image(NAME_IMAGE_1, new_image)
+                return False
+            else:
+                copy_image(NAME_IMAGE_1, NAME_IMAGE_2)
+                save_image(NAME_IMAGE_1, new_image)
+                return True
+    else:
+        if not image_exists(NAME_IMAGE_1):
+            save_image(NAME_IMAGE_1, new_image)
+        else:
+            copy_image(NAME_IMAGE_1, NAME_IMAGE_2)
+            save_image(NAME_IMAGE_1, new_image)
+        
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4000)
